c618488e578f7a8bf20b4bccb6d4f3fa294fa62e
ANMOL-319 <sandhyastha002@gmail.com>
2024-08-17 08:48:55 +0545

    first

---
+++ src/js/swiper.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: false,
+  spaceBetween: 19,
+  slidesPerView: 'auto',
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
